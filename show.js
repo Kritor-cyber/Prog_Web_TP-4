c26e4a9555525e349c7214af9a518b23af69451c
Kritor-cyber <thibault.denier@efrei.net>
2021-05-04 17:02:01 +0200

    Question 1

--- show.js
+++ show.js
@@ -12,3 +12,10 @@ function showTasks()
     document.getElementById("tableauUtilisateurs").style.display = "none";
     document.getElementById("tableauTaches").style.display = "block";
 }
+
+function getTasks()
+{
+    fetch("https://jsonplaceholder.typicode.com/todos").then(response => response.json()).then(function(data) {
+        console.log('data', data);
+    });
+}

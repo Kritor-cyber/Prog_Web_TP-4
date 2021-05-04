622151a92d70a70b5700c87e4506f951c89d7102
Kritor-cyber <thibault.denier@efrei.net>
2021-05-04 16:53:53 +0200

    Question 6

--- show.js
+++ show.js
@@ -1,10 +1,11 @@
 function showUsers()
 {
-    document.getElementById("tableauUtilisateurs").style.display = "block";
+    document.getElementById("tableauUtilisateurs").style.display = "table";
     document.getElementById("tableauTaches").style.display = "none";
 }
 
 function showTasks()
 {
-
+    document.getElementById("tableauUtilisateurs").style.display = "none";
+    document.getElementById("tableauTaches").style.display = "block";
 }
